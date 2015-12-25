b2921c6ac9541d9eb12db0390eb517248a4e5d7a
etkirsch <etkirsch@gmail.com>
2015-12-25 12:43:48 -0800

    Added /process/<text>

--- launch.py
+++ launch.py
@@ -1,9 +1,17 @@
+from autoambience.KeywordIdentifier import KeywordIdentifier
 from flask import Flask
+
+ki = KeywordIdentifier()
 app = Flask(__name__)
 
 @app.route('/')
-def launch():
+def nothing_interesting():
     return '"launch" flask application running'
+
+@app.route('/process/<text>')
+def process(text):
+    return ki.process(text)
+
 
 if __name__ == '__main__':
     app.run()
